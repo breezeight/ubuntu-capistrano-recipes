4e7b3c3f735a800ee688e3ab8355d036a5f7f5e5
Nicola Brisotto <nicolabrisotto@gmail.com>
2008-12-03 17:04:36 +0100

    refactor without deprec

--- recipes/recipes.rb
+++ recipes/recipes.rb
@@ -1,3 +1,59 @@
+namespace :mysql do
+	desc "Create user granted to DROP/CREATE rails databases"
+	task :create_rails_user do
+    envs = ["development", "test", "production"]
+    envs.each do |env|
+      run "mysql mysql -u #{db_root_user} -p -e 'grant all on #{application}_#{env}.* to #{user}@localhost;' " do |channel, stream, data|
+        channel.send_data "#{db_root_pw}\n"
+        puts data
+      end
+    end
+	end
+end
+
+namespace :dm_deploy do
+  desc "Create all databases on the primary db server. Setup the database.yml file"
+  task :create_databases, :roles => :db, :only => { :primary => true } do
+    #1 config database.yml
+    envs = ["development", "test", "production"]
+    database_configuration = ""
+    envs.each do |env|
+    database_configuration += <<-EOF
+#{env}:
+    adapter: mysql
+    socket: /var/run/mysqld/mysqld.sock
+    username: #{user}
+    password: 
+    database: #{application}_#{env}
+  
+    EOF
+    end
+    put database_configuration, "#{current_path}/config/database.yml"
+    
+    #2 create mysql users
+    mysql.create_rails_user
+    
+    #3 create databases
+    rake = fetch(:rake, "rake")
+    rails_env = fetch(:rails_env, "production")
+    migrate_env = fetch(:migrate_env, "")
+    migrate_target = fetch(:migrate_target, :latest)
+
+    directory = case migrate_target.to_sym
+      when :current then current_path
+      when :latest  then current_release
+      else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+      end
+    run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:create:all"
+    
+  end
+  
+  desc "Set application directory user and permission"
+  task :set_app_dir_access, :roles => :app do
+    sudo "chown #{user}:#{user} -R #{deploy_to}"
+  end
+end
+
 namespace :ubuntu804 do
     desc "Install Git"
     task :install_git do
@@ -6,21 +62,21 @@ namespace :ubuntu804 do
 
     desc "Install Ruby"
     task :install_ruby do
-        apt.install({:base => %w(ruby=4.1 rdoc=4.1 libopenssl-ruby=4.1 ruby1.8-dev)}, :stable)
+        sudo "apt-get install ruby1.8 libopenssl-ruby1.8 ruby1.8-dev"
     end
 
     desc "Install Rails"
     task :install_rails do
-        apt.install( {:base => %w(libmysqlclient15-dev sqlite3 libsqlite3-ruby libsqlite3-dev)}, :stable )
-        gem2.install 'sqlite3-ruby'
-        gem2.install 'mysql'
-        gem2.install 'rails', '2.1.0'
-        gem2.install 'rspec'
+        sudo "apt-get install libmysqlclient15-dev sqlite3 libsqlite3-ruby libsqlite3-dev"
+        sudo "gem install sqlite3-ruby"
+        sudo "gem install mysql"
+        sudo "gem install rails-2.1.0"
+        sudo "gem install rspec"
     end
 
     desc "Install Ruby"
     task :install_mysql do
-        apt.install( {:base => %w(mysql-server-5.0 mysql-client-5.0)}, :stable )
+        sudo "apt-get install mysql-server-5.0 mysql-client-5.0"
     end
     namespace :apache do
         desc "Install Apache"
@@ -29,14 +85,14 @@ namespace :ubuntu804 do
         end
         desc "Install Passenger"
         task :install_passenger do
-            gem2.install 'passenger'
+            sudo "gem install passenger"
             print "TODO run passenger-install-apache2-module"
-            #            input = ''
-            #            run "sudo passenger-install-apache2-module" do |ch, stream, out|
-            #                next if out.chomp == input.chomp || out.chomp == ''
-            #                print out
-            #                ch.send_data(input = $stdin.gets) if out =~ /enter/i
-            #            end
+            input = ''
+            run "sudo passenger-install-apache2-module" do |ch, stream, out|
+              next if out.chomp == input.chomp || out.chomp == ''
+              print out
+              ch.send_data(input = $stdin.gets) if out =~ /enter/i
+            end
         end
 
         desc "Configure Passenger"
@@ -55,12 +111,12 @@ EOF
             vhost_config =<<-EOF
 <VirtualHost *:80>
 ServerName #{server_address}
-DocumentRoot #{deploy_to}/current/public
+DocumentRoot #{deploy_to}current/public
 </VirtualHost>
 EOF
             put vhost_config, "vhost_config"
-            sudo "mv vhost_config /etc/apache2/sites-available/#{application}"
-            sudo "a2ensite #{application}"
+            sudo "mv vhost_config /etc/apache2/sites-available/#{application}_#{user}"
+            sudo "a2ensite #{application}_#{user}"
         end
     end
 end
